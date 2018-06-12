ea53c80fa2129078e42e5179527b60ee433597d1
hup <hup@ipampas.com>
2018-06-12 10:13:42 +0800

    dockerfile

--- hup-platform-mail/mail-server/src/main/java/com/hup/platform/mail/server/MailApplication.java
+++ hup-platform-mail/mail-server/src/main/java/com/hup/platform/mail/server/MailApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 @SpringCloudApplication
-public class Application {
+public class MailApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(MailApplication.class, args);
     }
 }
--- hup-platform-storage/storage-server/src/main/java/com/hup/platform/storage/server/StorageApplication.java
+++ hup-platform-storage/storage-server/src/main/java/com/hup/platform/storage/server/StorageApplication.java
@@ -1,13 +1,12 @@
 package com.hup.platform.storage.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 @SpringCloudApplication
-public class Application {
+public class StorageApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(StorageApplication.class, args);
     }
 }
