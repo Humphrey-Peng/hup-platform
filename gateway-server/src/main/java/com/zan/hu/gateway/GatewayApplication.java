252693b89dbc0637eea8229d2fa28975e54211c7
jingxing <m17364550730@163.com>
2019-04-04 14:31:21 +0800

    配置一些基础服务的网关

--- gateway-server/src/main/java/com/zan/hu/gateway/GatewayApplication.java
+++ gateway-server/src/main/java/com/zan/hu/gateway/GatewayApplication.java
@@ -1,10 +1,18 @@
 package com.zan.hu.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
 /**
  * @version 1.0
  * @Author hupeng
  * @Date 2019-04-03 21:05
  * @Description todo
  **/
+@SpringCloudApplication
 public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
