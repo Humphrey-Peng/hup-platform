4534d7326f36371065e2ca09e757808b1fa2adcb
jingxing <m17364550730@163.com>
2019-04-24 20:01:40 +0800

    增加分布式配置中心

--- config-server/src/main/java/com/zan/hu/config/ConfigApplication.java
+++ config-server/src/main/java/com/zan/hu/config/ConfigApplication.java
@@ -1,10 +1,19 @@
 package com.zan.hu.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * @version 1.0
  * @Author hupeng
  * @Date 2019-04-03 21:04
  * @Description todo
  **/
+@SpringCloudApplication
+@EnableConfigServer
 public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
 }
